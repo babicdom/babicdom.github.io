97ef37a199e45eb8bad7973b65ca0c821a4377df
babe1304 <babidominik26@gmail.com>
2025-11-22 19:43:05 +0100

    Old tailwind with astro fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,9 @@
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   output: 'static',
   site: 'https://babicdom.github.io',
-  integrations: [tailwind()],
   base: '/',
 
   build: {
